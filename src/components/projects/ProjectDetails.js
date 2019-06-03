3047cdc12c7ab9458088c97cf49b1184b7fe4b3b
ch264 <christinavhastenrath@gmail.com>
2019-06-03 15:01:28 -0700

    grab params and output to template

--- src/components/projects/ProjectDetails.js
+++ src/components/projects/ProjectDetails.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const ProjectDetails = () => {
+const ProjectDetails = (props) => {
+	const id = props.match.params.id;
 	return (
 		<div>
 			<div className="container section project-details">
 				<div className="card z-depth-0">
 					<div className="card-content">
-						<span className="card-title">Project Title</span>
+						<span className="card-title">Project Title - {id}</span>
 						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure esse, natus, pariatur molestias amet mollitia maiores aperiam excepturi voluptate vitae, magnam quia corporis repudiandae quae provident eligendi quibusdam. Labore, odit!</p>
 					</div>
 					<div className="card-action grey lighten-4 grey-text">

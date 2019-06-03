dd1467eb16319d21d047003f88a55eb27ec6005b
ch264 <christinavhastenrath@gmail.com>
2019-06-03 14:58:38 -0700

    added Project Details route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Dashboard from'./components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
 
 
 function App() {
@@ -10,7 +11,8 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path='/' component={Dashboard} />
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/project/:id' component={ProjectDetails} />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/components/projects/ProjectDetails.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ProjectDetails = () => {
+	return (
+		<div>
+			<div className="container section project-details">
+				<div className="card z-depth-0">
+					<div className="card-content">
+						<span className="card-title">Project Title</span>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure esse, natus, pariatur molestias amet mollitia maiores aperiam excepturi voluptate vitae, magnam quia corporis repudiandae quae provident eligendi quibusdam. Labore, odit!</p>
+					</div>
+					<div className="card-action grey lighten-4 grey-text">
+						<div>Posted by CH</div>
+						<div>Date</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default ProjectDetails

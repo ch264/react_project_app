b66da280b287a3d4ea0f6de29628243e96e84233
ch264 <christinavhastenrath@gmail.com>
2019-06-03 15:21:50 -0700

    created signup component and linked signin and sign up with route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layouts/Navbar';
 import Dashboard from'./components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails'
 import SignIn from './components/auth/SignIn'
+import SignUp from './components/auth/SignUp'
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route exact path='/' component={Dashboard} />
           <Route path='/project/:id' component={ProjectDetails} />
           <Route path='/signin' component={SignIn} />
+          <Route path='/signup' component={SignUp} />
         </Switch>
       </div>
     </BrowserRouter>

24be5333e0d49046c322fdd3dc523b0d4c428af8
ch264 <christinavhastenrath@gmail.com>
2019-06-03 15:46:40 -0700

    linked up nav links

--- src/components/layouts/SignedInNav.js
+++ src/components/layouts/SignedInNav.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 const SignedInNav = () => {
 	return (
 		<ul className="right">
-			<li><NavLink to='/'>New Project</NavLink></li>
+			<li><NavLink to='/create'>New Project</NavLink></li>
 			<li><NavLink to='/'>Log Out</NavLink></li>
 			<li><NavLink to='/' className='btn btn-floating red lighten-2'>CH</NavLink></li>
 		</ul>

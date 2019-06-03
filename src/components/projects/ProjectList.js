a34b5385d0045c144e18a9a34b4c272e9adf0d4f
ch264 <christinavhastenrath@gmail.com>
2019-06-03 14:45:49 -0700

    added project summary and refractored project list

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,32 +1,14 @@
 import React from 'react'
+import ProjectSummary from './ProjectSummary'
 
 const ProjectList = () => {
 	return (
 		<div className="project-list section">
 
-			<div className="card z-depth-0 project-summary">
-				<div className="card-content grey-text-darken-3">
-					<span className="card-title">Project Title</span>
-					<p>Posted by CH</p>
-					<p className='grey-text'>June 3rd</p>
-				</div>
-			</div>
-
-			<div className="card z-depth-0 project-summary">
-				<div className="card-content grey-text-darken-3">
-					<span className="card-title">Project Title</span>
-					<p>Posted by CH</p>
-					<p className='grey-text'>June 3rd</p>
-				</div>
-			</div>
-
-			<div className="card z-depth-0 project-summary">
-				<div className="card-content grey-text-darken-3">
-					<span className="card-title">Project Title</span>
-					<p>Posted by CH</p>
-					<p className='grey-text'>June 3rd</p>
-				</div>
-			</div>
+			<ProjectSummary />
+			<ProjectSummary />
+			<ProjectSummary />
+			<ProjectSummary />
 
 		</div>
 	)
---
+++ src/components/projects/ProjectSummary.js
@@ -0,0 +1,16 @@
+// summary of a single project
+import React from 'react'
+
+const ProjectSummary = () => {
+	return (
+		<div className="card z-depth-0 project-summary">
+				<div className="card-content grey-text-darken-3">
+					<span className="card-title">Project Title</span>
+					<p>Posted by CH</p>
+					<p className='grey-text'>June 3rd</p>
+				</div>
+			</div>
+	)
+}
+
+export default ProjectSummary

3d207503e7c21964e1a48cdac32dabda9ffdcd31
ch264 <christinavhastenrath@gmail.com>
2019-06-04 08:45:25 -0700

    complete user signin with firebase auth

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { signIn } from '../../store/actions/authActions'
 
 class SignIn extends Component {
 	state = {
@@ -14,10 +16,11 @@ class SignIn extends Component {
 	}
 	handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(this.state)
+		this.props.signin(this.state)
 	}
 
 	render() {
+		const { authError } = this.props;
 		return (
 			<div className='container'>
 				<form onSubmit={this.handleSubmit} className="white">
@@ -32,6 +35,9 @@ class SignIn extends Component {
 					</div>
 					<div className="input-field">
 						<button className="btn red lighten-3 z-depth-0">Login</button>
+						<div className="red-text center">
+							{ authError ? <p>{authError}</p> : null }
+						</div>
 					</div>
 				</form>
 			</div>
@@ -39,4 +45,20 @@ class SignIn extends Component {
 	}
 }
 
-export default SignIn
+const mapStateToProps = (state) => {
+	return {
+		// state.auth comes from the root reducer
+		authError: state.auth.authError
+	}
+}
+ 
+// map dispatch to props so that we have access to signin mathod from action creator
+const mapDispatchToProps = (dispatch) => {
+	return {
+		signin: (credentials) => dispatch(signIn(credentials))
+	}
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn)
--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,9 +1,26 @@
 
-const initState = {}
+const initState = {
+	authError: null
+}
 
 // manipulates the state
 const authReducer = (state = initState, action) => {
-	return state
+	switch(action.type) {
+		case 'LOGIN_ERROR':
+			console.log('login error')
+			return {
+				...state,
+				authError: 'failed login'
+			}
+			case 'LOGIN_SUCCESS':
+				console.log('login success')
+				return {
+					...state,
+					authError:null
+				}
+			default:
+				return state;
+	}
 }
 
 export default authReducer

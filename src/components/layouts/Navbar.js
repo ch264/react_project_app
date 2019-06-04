729a28573995f9a172382ad40c5944f0c8d5ec16
ch264 <christinavhastenrath@gmail.com>
2019-06-04 09:01:37 -0700

    tracking auth status and showing differnt components dependent on the auth status

--- src/components/layouts/Navbar.js
+++ src/components/layouts/Navbar.js
@@ -4,22 +4,24 @@ import SignedInNav from './SignedInNav'
 import SignedOutNav from './SignedOutNav'
 import { connect } from 'react-redux'
  
-const Navbar = () => {
+const Navbar = (props) => {
+	const { auth } = props;
+	// save component in links depending on auth status
+	const links = auth.uid ? <SignedInNav /> : <SignedOutNav />
 	return (
 		<nav className="nav-wrapper grey darken-1">
 			<div className="container">
 				<Link to='/' className='brand-logo left'>Project Planner</Link>
-				<SignedInNav />
-				<SignedOutNav />
+				{/* depending on sign in status will show either link */}
+				{ links }
 			</div>
 		</nav>
 	)
 }
 
 const mapStateToProps = (state) => {
-	console.log(state)
 	return {
-
+		auth: state.firebase.auth
 	}
 }
 

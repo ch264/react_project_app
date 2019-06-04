31f47f1e856930935f8870ed7ff69391ef6cf7ce
ch264 <christinavhastenrath@gmail.com>
2019-06-03 20:22:22 -0700

    create project in project action, using thunk. pause dispatch and add new document to firebase, hardcoding for now, then dispatch when complete and catch in any errors that are handles in switch case. state is just returned for now

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -3,7 +3,19 @@
 export const createProject = (project) =>{
 	// dispatch method dispatches an action to the reducer
 	return (dispatch, getState, { getFirebase, getFirestore }) => {
-		// pause dispatch and make async call to database
-		dispatch({ type: 'CREATE_PROJECT', project: project});
+		const firestore = getFirestore();
+		// returns promise because it takes time
+		firestore.collection('projects').add({
+			...project,
+			authorFirstName:'Christina',
+			authorLastName: 'Muller',
+			authorId: 23423,
+			createdAt: new Date()
+		}).then(() => {
+			// pause dispatch and make async call to database
+			dispatch({ type: 'CREATE_PROJECT', project: project});
+		}).catch((err) => {
+			dispatch({ type: 'CREATE_PROJECT_ERROR', err});
+		})
 	}
 };

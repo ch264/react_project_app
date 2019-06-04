4492a6686da2372d517419dfb881de781071596b
ch264 <christinavhastenrath@gmail.com>
2019-06-04 08:54:04 -0700

    setup user logout

--- src/components/layouts/SignedInNav.js
+++ src/components/layouts/SignedInNav.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
- 
-const SignedInNav = () => {
+import { connect } from 'react-redux'
+import { signOut } from '../../store/actions/authActions'
+
+// functional components take in props
+const SignedInNav = (props) => {
 	return (
 		<ul className="right">
 			<li><NavLink to='/create'>New Project</NavLink></li>
-			<li><NavLink to='/'>Log Out</NavLink></li>
+			<li><a onClick={props.signOut}>Log Out</a></li>
 			<li><NavLink to='/' className='btn btn-floating red lighten-2'>CH</NavLink></li>
 		</ul>
 	)
 }
 
-export default SignedInNav
+const mapDispatchToProps = (dispatch) => {
+	return {
+		signOut: () => dispatch(signOut())
+	}
+}
+
+export default connect(null, mapDispatchToProps)(SignedInNav)

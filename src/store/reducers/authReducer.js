28293d40818d04f330802b8f67d028dfa9542a08
ch264 <christinavhastenrath@gmail.com>
2019-06-04 10:31:39 -0700

    completed user signup with firebase and creating a new user entry in firestore

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,3 +1,4 @@
+// handle dispatch in reducer
 
 const initState = {
 	authError: null
@@ -6,23 +7,35 @@ const initState = {
 // manipulates the state
 const authReducer = (state = initState, action) => {
 	switch(action.type) {
+		case 'SIGNUP_SUCCESS':
+			console.log('signup success')
+			return {
+				...state,
+				authError: null
+			}
+		case 'SIGNUP_ERROR': 
+			console.log('signup error')
+			return {
+				...state,
+				authError: action.err.message
+			}
+		case 'LOGIN_SUCCESS':
+				console.log('login success')
+				return {
+					...state,
+					authError:null
+				}
 		case 'LOGIN_ERROR':
 			console.log('login error')
 			return {
 				...state,
 				authError: 'failed login'
 			}
-			case 'LOGIN_SUCCESS':
-				console.log('login success')
-				return {
-					...state,
-					authError:null
-				}
-			case 'SIGNOUT_SUCCESS':
-				console.log('signout success')
-				return state;
-			default:
-				return state;
+		case 'SIGNOUT_SUCCESS':
+			console.log('signout success')
+			return state;
+		default:
+			return state;
 	}
 }
 

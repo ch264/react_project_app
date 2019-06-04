b663abadf99b7de4c4b4dce7286dbfb11dd9076a
ch264 <christinavhastenrath@gmail.com>
2019-06-03 20:07:54 -0700

    added firebase connection

--- src/index.js
+++ src/index.js
@@ -3,17 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 // redux middleware
 import thunk from 'redux-thunk'
 
-import { getFirestore } from 'redux-firestore'
-import { getFirebase } from 'react-redux-firebase'
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+
+import firebase from './config/firebase'
 
 // allows extra arguent in project actions
-const store = createStore(rootReducer, applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})));
+const store = createStore(rootReducer, 
+	compose(
+	applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+	// store enhancers to pass in config so that in projectActioins the database is known
+	reduxFirestore(firebase),
+	reactReduxFirebase(firebase)
+	)
+);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -2,7 +2,7 @@
 
 export const createProject = (project) =>{
 	// dispatch method dispatches an action to the reducer
-	return (dispatch, getState { getFirebase, getFirestore }) => {
+	return (dispatch, getState, { getFirebase, getFirestore }) => {
 		// pause dispatch and make async call to database
 		dispatch({ type: 'CREATE_PROJECT', project: project});
 	}
